41bfaacf4a311875a1e536d032cf0f0fe8ac49f3
D1sl <benjaminmolini@gmail.com>
2022-01-27 09:07:49 -0600

    Added ID's for individual dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,15 @@
 // Replace this once my own key starts working
 var apiKey = "b262298fbe39ad30d243f31f6e1297bc";
-var citySearch = "Detroit";
+var citySearch = "Helsinki";
 
 var lat = "";
 var lon = "";
-
-// var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey;
-
-
 
 var currentCityNameEl = document.querySelector("#cityname");
 var currentTempEl = document.querySelector("#currTemp");
 var currentHumidityEl = document.querySelector("#currHumidity");
 var currentWindEl = document.querySelector("#currWind");
 var currentUvEl = document.querySelector("#currUv");
-
-// Get weather info based on geoCoordinates function's output
-var getWeather = function (weatherCity) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" +  "&appid=" + apiKey;
-
-    console.log(apiUrl);
-
-    fetch(apiUrl)
-        .then(function (response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    console.log(data);
-                    currentTempEl.textContent = "Temperature: " + data.current.temp + " °F";
-                    currentWindEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
-                    currentHumidityEl.textContent = "Humidity: " + data.current.humidity + "%";
-                    currentUvEl.textContent = data.current.uvi*100 + "%";
-                })
-            }
-        })
-
-}
-
 
 // Fetch latitude and longitude for the search term
 var geoCoordinates = function () {
@@ -59,6 +33,39 @@ var geoCoordinates = function () {
 
 }
 
+// Get weather info based on geoCoordinates function's output
+var getWeather = function (weatherCity) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" +  "&appid=" + apiKey;
+
+    console.log(apiUrl);
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    currentTempEl.textContent = "Temperature: " + data.current.temp + " °F";
+                    currentWindEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
+                    currentHumidityEl.textContent = "Humidity: " + data.current.humidity + "%";
+                    currentUvEl.textContent = data.current.uvi*100 + "%";
+                    fiveDayForecast();
+                })
+            }
+        })
+
+}
+
+var fiveDayForecast = function(forecast) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" +  "&appid=" + apiKey;
+    
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                })
+            }
+        })
+}
 
 
 
