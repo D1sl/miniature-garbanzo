49b9e8e1e3665c77208d841e9c9f54e5b54c008a
D1sl <benjaminmolini@gmail.com>
2022-01-27 08:57:48 -0600

    Get weather data according to coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,12 @@ var lon = "";
 var currentCityNameEl = document.querySelector("#cityname");
 var currentTempEl = document.querySelector("#currTemp");
 var currentHumidityEl = document.querySelector("#currHumidity");
+var currentWindEl = document.querySelector("#currWind");
 var currentUvEl = document.querySelector("#currUv");
 
-
-
+// Get weather info based on geoCoordinates function's output
 var getWeather = function (weatherCity) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" +  "&appid=" + apiKey;
 
     console.log(apiUrl);
 
@@ -26,7 +26,10 @@ var getWeather = function (weatherCity) {
             if (response.ok) {
                 response.json().then(function (data) {
                     console.log(data);
-
+                    currentTempEl.textContent = "Temperature: " + data.current.temp + " °F";
+                    currentWindEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
+                    currentHumidityEl.textContent = "Humidity: " + data.current.humidity + "%";
+                    currentUvEl.textContent = data.current.uvi*100 + "%";
                 })
             }
         })
@@ -43,15 +46,12 @@ var geoCoordinates = function () {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
 
                     // Set city name to header
                     currentCityNameEl.textContent = data[0].name;
 
                     lat = data[0].lat;
                     lon = data[0].lon;
-                    console.log(lat);
-                    console.log(lon);
                     getWeather();
                 })
             }
