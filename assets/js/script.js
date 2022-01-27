d2ce1295db4f8cc619b50e59400f6a1a17843b3d
D1sl <94911312+D1sl@users.noreply.github.com>
2022-01-27 12:40:27 -0600

    Added form submit handler and 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,17 @@
 // Replace this once my own key starts working
 var apiKey = "b262298fbe39ad30d243f31f6e1297bc";
-var citySearch = "Helsinki";
-
-var lat = "";
-var lon = "";
 
 var currentCityNameEl = document.querySelector("#cityname");
 var currentTempEl = document.querySelector("#currTemp");
 var currentHumidityEl = document.querySelector("#currHumidity");
 var currentWindEl = document.querySelector("#currWind");
 var currentUvEl = document.querySelector("#currUv");
+var searchFormEl = document.querySelector("#searchform");
+var searchFieldEl = document.querySelector("#city");
 
 // Fetch latitude and longitude for the search term
-var geoCoordinates = function () {
-    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + citySearch + "&appid=" + apiKey;
+var geoCoordinates = function (city) {
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
     console.log(apiUrl);
 
     fetch(apiUrl)
@@ -23,22 +21,17 @@ var geoCoordinates = function () {
 
                     // Set city name to header
                     currentCityNameEl.textContent = data[0].name;
-
                     lat = data[0].lat;
                     lon = data[0].lon;
-                    getWeather();
-                })
-            }
-        })
-
-}
+                    getWeather(lat, lon);
+                });
+            };
+        });
+};
 
 // Get weather info based on geoCoordinates function's output
-var getWeather = function (weatherCity) {
+var getWeather = function (lat, lon) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" +  "&appid=" + apiKey;
-
-    console.log(apiUrl);
-
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
@@ -48,28 +41,48 @@ var getWeather = function (weatherCity) {
                     currentHumidityEl.textContent = "Humidity: " + data.current.humidity + "%";
                     currentUvEl.textContent = data.current.uvi*100 + "%";
                     fiveDayForecast();
-                })
-            }
-        })
-
-}
+                });
+            };
+        });
+};
 
 var fiveDayForecast = function(forecast) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" +  "&appid=" + apiKey;
-    
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
-                })
-            }
-        })
-}
+                    document.querySelector("#t1").textContent = data.daily[1].temp.day;
+                    document.querySelector("#t2").textContent = data.daily[2].temp.day;
+                    document.querySelector("#t3").textContent = data.daily[3].temp.day;
+                    document.querySelector("#t4").textContent = data.daily[4].temp.day;
+                    document.querySelector("#t5").textContent = data.daily[5].temp.day;
+                    
+                    document.querySelector("#w1").textContent = data.daily[1].wind_speed;
+                    document.querySelector("#w2").textContent = data.daily[2].wind_speed;
+                    document.querySelector("#w3").textContent = data.daily[3].wind_speed;
+                    document.querySelector("#w4").textContent = data.daily[4].wind_speed;
+                    document.querySelector("#w5").textContent = data.daily[5].wind_speed;
+                    
+                    document.querySelector("#h1").textContent = data.daily[1].humidity;
+                    document.querySelector("#h2").textContent = data.daily[2].humidity;
+                    document.querySelector("#h3").textContent = data.daily[3].humidity;
+                    document.querySelector("#h4").textContent = data.daily[4].humidity;
+                    document.querySelector("#h5").textContent = data.daily[5].humidity;
+                });
+            };
+        });
+};
 
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    citySearch = searchFieldEl.value.trim();
+    if (citySearch) {
+        geoCoordinates(citySearch);
+        searchFieldEl.value = "";
+    } else {
+        alert("Please enter a search term");
+    };
+};
 
-
-geoCoordinates();
-
-
-// getWeatherData();
+searchFormEl.addEventListener("submit", formSubmitHandler);
